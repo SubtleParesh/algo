3122a4c9f65dd9660c47725eaadb1524f46d5ebd
Paresh <subtleparesh@gmail.com>
2021-06-19 00:06:30 +0530

    Custom configs

---
+++ cloud_init.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+export METHOD=cloud
+export ONDEMAND_CELLULAR=true
+export ONDEMAND_WIFI=false
+export STORE_PKI=false 
+export DNS_ADBLOCKING=true
+export SSH_TUNNELING=false
+export REPO_SLUG=SubtleParesh/algo
+export USERS=homeserver,homewindows,worklaptop,oneplus7,edward,alphonse,simba,hermione,pikachu,wolverine,merlin,mario,cloud,yoda,alice,loki
+curl -s https://raw.githubusercontent.com/SubtleParesh/algo/master/install.sh | sudo -E bash -x
