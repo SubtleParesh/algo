d506f5493c88b790578e0dff2fb19048f900a36a
Paresh <subtleparesh@gmail.com>
2021-06-30 22:16:02 +0530

    Update Config to allow computers to connect

--- cloud_init.bash
+++ cloud_init.bash
@@ -5,6 +5,7 @@ export ONDEMAND_WIFI=false
 export STORE_PKI=false 
 export DNS_ADBLOCKING=true
 export SSH_TUNNELING=false
+export ENDPOINT=`wget http://ipecho.net/plain -O - -q ; echo`
 export REPO_SLUG=SubtleParesh/algo
 export USERS=homeserver,homewindows,worklaptop,oneplus7,edward,alphonse,simba,hermione,pikachu,wolverine,merlin,mario,cloud,yoda,alice,loki
 curl -s https://raw.githubusercontent.com/SubtleParesh/algo/master/install.sh | sudo -E bash -x
